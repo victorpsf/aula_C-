81f45e0139d839c673af2e2b85b5fe236356bfc9
victorpsf <victorpsf2@hotmail.com>
2021-12-13 21:36:43 -0300

    classe padrão inicial

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Aula {
+    public class Program {
+
+        // ponto de acesso da aplicação
+        public static void Main(string[] arguments) { }
+    }
+}
